9fce63309f46de82f658ac6a4fadb32d9df221ec
SkSumit <kolsum24@gmail.com>
2020-11-07 19:03:47 +0530

    Refactored Code

--- src/main/java/homeScreen/Backend.java
+++ src/main/java/homeScreen/Backend.java
@@ -1,46 +1,47 @@
-
 package homeScreen;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JOptionPane;
-
 public class Backend {
-    public Backend(){
+
+    private Databaseconn databaseconn;
+
+    public Backend() {
+        databaseconn = new Databaseconn();
         System.out.println("Hello & Welcome to Constructor");
     }
-    
-    public int Register(JTextField jTextField1,JTextField jTextField2,JTextField jTextField3,JTextField jTextField4,JTextField jTextField5,JTextField jTextField6,JComboBox jComboBox1){
-        String Fname = jTextField1.getText();
-        String Lname = jTextField2.getText();
-        String mobStr = jTextField3.getText();
-        String email = jTextField4.getText();
-        String pass = jTextField5.getText();
-        String confpasas = jTextField6.getText();
-        Object gender =  jComboBox1.getSelectedItem();
-        String realgender = gender.toString();
-        String insert = "INSERT INTO Users(FirstName , LastName , Mobile , Gender , Email , Pass) VALUES ('"+Fname+"' , '"+Lname+"' , '"+mobStr+"' , '"+realgender+"' , '"+email+"', SHA('"+pass+"'))";
-        String select = "SELECT Email FROM USERS WHERE Email = '"+email+"'";
-        Databaseconn db = new Databaseconn(); // Creating object of class type Databaseconn
-        Statement stat = db.getStat();
 
-        try {
-            ResultSet exist = stat.executeQuery(select);
-            if(exist.next()){
-                System.out.println("Record already exist");
-                return 0;
-            }
-            else{
-                stat.executeUpdate(insert);
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return 1;
+//    public int Register(JTextField jTextField1,JTextField jTextField2,JTextField jTextField3,JTextField jTextField4,JTextField jTextField5,JTextField jTextField6,JComboBox jComboBox1){
+//        String Fname = jTextField1.getText();
+//        String Lname = jTextField2.getText();
+//        String mobStr = jTextField3.getText();
+//        String email = jTextField4.getText();
+//        String pass = jTextField5.getText();
+//        String confpasas = jTextField6.getText();
+//        Object gender =  jComboBox1.getSelectedItem();
+//        String realgender = gender.toString();
+//        String insert = "INSERT INTO Users(FirstName , LastName , Mobile , Gender , Email , Pass) VALUES ('"+Fname+"' , '"+Lname+"' , '"+mobStr+"' , '"+realgender+"' , '"+email+"', SHA('"+pass+"'))";
+//        String select = "SELECT Email FROM USERS WHERE Email = '"+email+"'";
+//        Databaseconn db = new Databaseconn(); // Creating object of class type Databaseconn
+//        Statement stat = db.getStat();
+//
+//        try {
+//            ResultSet exist = stat.executeQuery(select);
+//            if(exist.next()){
+//                System.out.println("Record already exist");
+//                return 0;
+//            }
+//            else{
+//                stat.executeUpdate(insert);
+//            }
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//        return 1;
+//    }
+    public int Register(String firstName, String lastName,
+             String mobile, String gender, String email, String password, String confirmedPassword) {
+
+        int user = databaseconn.postSignUpUser(firstName, lastName, mobile, gender, email,
+                 password, confirmedPassword);
+        return user;
     }
 }
--- src/main/java/homeScreen/Databaseconn.java
+++ src/main/java/homeScreen/Databaseconn.java
@@ -1,36 +1,88 @@
-
 package homeScreen;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 //Class Creation
+
 public class Databaseconn {
-      private Connection conn;
-      private Statement stat;
-    //Constructor
+
+    private Connection conn;
+    private Statement statement;
+    private String connectionUsername = "root";
+    private String connectionPassword = "";
+    private String databaseName = "homeffins";
+
+    //SQL QUERIES
+    private static final String GET_ALL_USERS = "select * from restro.custdata";
+
+    //Constructor and Innitialization
     public Databaseconn() {
-        
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/homeffins";
-            String user = "root";
-            String password = "";
-            conn = DriverManager.getConnection(url,user,password);
-            stat = conn.createStatement();
-            System.out.println("Connection Successfull");
-            System.out.println(stat);
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    //Function
-        public Statement getStat(){
-            return stat;
-        }
-        
-        
-    }
-    
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/restro",
+                    connectionUsername, connectionPassword);
+            System.out.println("Connected");
+            statement = conn.createStatement();
 
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    //Return results after SQL Operation
+    private ResultSet ExecuteOperation(String query) throws SQLException {
+        return statement.executeQuery(query);
+    }
+
+    private int ExecuteUpdateOperation(String query) throws SQLException {
+        return statement.executeUpdate(query);
+    }
+
+    //GET Registered Users from DB
+    public ArrayList<String> getUsers() {
+        ArrayList<String> userList = new ArrayList<String>();
+
+        try {
+            var resultSet = ExecuteOperation(GET_ALL_USERS);
+            while (resultSet.next()) {
+                System.out.println(resultSet);
+                userList.add(resultSet.getString("name"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Databaseconn.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return userList;
+    }
+
+    public int postSignUpUser(String firstName, String lastName,
+             String mobile, String gender, String email, String password, String confirmedPassword) {
+
+        int user = 0;
+
+        String POST_USER_DATA
+                = "insert into " + databaseName + ".users (userFirstName, userLastName ,\n"
+                + "  userEmail ," + "  userGender ," + "  userPhoneNumber," + "  userPassword ) "
+                + "values ("
+                + "'" + firstName + "' , '" + lastName + "' , '" + email + "' , '" + gender + "' , "
+                + "'" + mobile + "', SHA('" + password + "'))";
+
+        try {
+            System.out.println(POST_USER_DATA);
+            var resultSet = ExecuteUpdateOperation(POST_USER_DATA);
+            System.out.println(resultSet);
+            user = resultSet;
+        } catch (SQLException ex) {
+            Logger.getLogger(Databaseconn.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return user;
+    }
+
+}
